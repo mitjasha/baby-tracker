dd4e4504d8b094c6ae3c5665248caa402ff1b0af
Zhanna Bohan <foto1drug@gmail.com>
2022-01-13 18:57:22 +0300

    refactor: add new folder for  ProgressBar

---
+++ src/components/common/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ProgressBar.css";
+
+const ProgressBar: React.FC = () => {
+    const now = 60;
+return(
+  <>
+      <ProgressBar now={60} />
+  </>
+);
+}
+
+export default ProgressBar;
