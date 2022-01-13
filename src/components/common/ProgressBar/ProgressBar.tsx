eee1b371c94cb645167711ead9abc67510de915f
Zhanna Bohan <foto1drug@gmail.com>
2022-01-14 00:30:54 +0300

    feat: add ProgressBar component and render it in SleepScreen

--- src/components/common/ProgressBar/ProgressBar.tsx
+++ src/components/common/ProgressBar/ProgressBar.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 import "./ProgressBar.css";
+import cnd from "classnames";
 
-const ProgressBar: React.FC = () => {
-    const now = 60;
-return(
+interface IProgressBar {
+  value: string;
+  max: string;
+  classNameProgressBar: string;
+  classNameContainer: string;
+  textName: string;
+  textValue: string;
+  icon: string;
+}
+
+const ProgressBar: React.FC<IProgressBar> = ({
+  classNameContainer,
+  max,
+  value,
+  classNameProgressBar,
+  textName,
+  textValue,
+  icon}) => (
   <>
-      <ProgressBar now={60} />
+  <div className= {classNameContainer}>
+    <div className={cnd("img-progress-bar", icon)}/>
+    <span>{textName}</span>
+      <progress max = {max} value={value} className= {cnd("progress-line", classNameProgressBar)}/>
+      <span>{textValue}</span>
+      </div>
   </>
 );
-}
 
 export default ProgressBar;
--- src/components/containers/SleepScreen/SleepScreen.tsx
+++ src/components/containers/SleepScreen/SleepScreen.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import ProgressBar from "../../common/ProgressBar/ProgressBar";
+import "./SleepScreen.css";
 
-const SleepScreen: React.FC = () => <div className="screen">Sleep Screen</div>;
+const SleepScreen: React.FC = () => (
+  <div className="screen">Sleep Screen
+    <ProgressBar 
+    icon = "night-icon"
+    value = "60"
+    max = "100"
+    classNameProgressBar = "sleep-night-progress"
+    classNameContainer = "progress-bar-container"
+    textName = "Ночной сон" 
+    textValue = "3 ч 30 м"
+    />
+    <ProgressBar
+    icon = "day-icon"
+    value = "30"
+    max = "100"
+    classNameProgressBar ="sleep-day-progress"
+    classNameContainer = "progress-bar-container"
+    textName = "Дневной сон"
+    textValue = "1 ч 30 м"
+    />
+</div>
+
+);
 
 export default SleepScreen;
