a579b91ddf7faeed0a49b9d2c8bc62d87747f14f
Dzmitry Shakhrai <romantus_@mail.ru>
2022-01-16 00:27:29 +0300

    feat: add burger menu template

---
+++ src/components/common/SideBar/SideBar.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import styled from "styled-components";
+import { IconContext } from "react-icons";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import SidebarData from "./SidebarData";
+import Submenu from "./Submenu";
+import "./SideBar.css";
+
+const Sidebar: React.FC = () => {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <div className="nav">
+        <Link to="#" className="nav-icon" onClick={showSidebar}>
+          <AiOutlineMenu />
+        </Link>
+      </div>
+      <div className="sidebar-nav" style={sidebar ? { left: "0%" } : { left: "-100%" }}>
+        <div className="sidebar-wrap">
+          <Link to="#" className="nav-icon" onClick={showSidebar}>
+            <AiOutlineClose />
+          </Link>
+          {SidebarData.map((item, index) => (
+            <Submenu item={item} key={index} />
+          ))}
+        </div>
+      </div>
+    </IconContext.Provider>
+  );
+};
+
+export default Sidebar;
--- src/components/containers/Header.tsx
+++ src/components/containers/Header.tsx
@@ -1,19 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import SideBar from "../common/SideBar/SideBar";
+// import { Link } from "react-router-dom";
+// import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+// import Button from "../common/Button/Button";
+import "./Header.css";
 
 const Header: React.FC = () => (
   <header className="header">
-    <div className="back ">
+    {/* <Navbar collapseOnSelect expand="xxl" className="header-nav">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <NavDropdown title="Ребенок" id="collasible-nav-dropdown" className="header-nav-collapse">
+        <NavDropdown.Item href="#action/3.1" className="header-nav-collapse">
+          <Navbar.Brand href="#home">Полина</Navbar.Brand>
+        </NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">
+          <Navbar.Brand href="#home">Андрей</Navbar.Brand>
+        </NavDropdown.Item>
+      </NavDropdown>
+
+      <div className="header-user"></div>
+      <div className="settings">
+        <Link to="/settings" className="settings-cog-btn" id="settings">
+          settings
+        </Link>
+      </div>
+      <Navbar.Collapse id="responsive-navbar-nav" className="header-nav-collapse">
+        <Nav className="mr-auto">
+          <Button onClick={() => onclick}>Сон</Button>
+          <Nav.Link href="#pricing">Кормление</Nav.Link>
+          <Nav.Link href="#pricing">Активности</Nav.Link>
+          <Nav.Link href="#pricing">Статистика</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar> */}
+    {/* <div className="back ">
       <Link to="/" className="back-btn" id="back">
         back
       </Link>
-    </div>
-    <div />
-    <div className="settings">
-      <Link to="/settings" className="settings-cog-btn" id="settings">
-        settings
-      </Link>
-    </div>
+    </div> */}
+    <SideBar></SideBar>
   </header>
 );
 
