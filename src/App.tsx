f15642c75e1132e3f923bfd6d6f3d0a0d784d5b0
Dzmitry Shakhrai <romantus_@mail.ru>
2022-02-17 18:31:57 +0300

    ref: dropdown component and reqiredAuth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import ActivityScreen from "./components/containers/ActivityScreen/ActivityScreen";
 import FeedingScreen from "./components/containers/FeedingScreen/FeedingScreen";
 import MainScreen from "./components/containers/MainScreen/MainScreen";
@@ -10,6 +10,7 @@ import RegScreen from "./components/containers/RegScreen/RegnScreen";
 import BabyDataScreen from "./components/containers/BabyDataScreen/BabyDataScreen";
 import Header from "./components/containers/Header";
 import Footer from "./components/containers/Footer";
+import RequireAuth from "./components/helpers/RequireAuth";
 
 const routesWithoutHeader: string[] = ["/", "/registration", "/baby-data"];
 
@@ -21,17 +22,6 @@ const App: React.FC = () => {
     setHeader(routesWithoutHeader.includes(location.pathname));
     setFooter(routesWithoutHeader.includes(location.pathname));
   }, [location.pathname]);
-
-  // eslint-disable-next-line react/prop-types
-  function RequireAuth({ children }: { children: React.ReactElement }) {
-    const authed = !!localStorage.getItem("accessToken");
-
-    return authed === true ? (
-      (children as React.ReactElement)
-    ) : (
-      <Navigate to="/#" replace />
-    );
-  }
 
   return (
     <>
--- src/components/common/DropDown/DropDown.tsx
+++ src/components/common/DropDown/DropDown.tsx
@@ -1,60 +1,62 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { IChild } from "../../../api/api.interface";
-import userController from "../../../api/userController";
 
-const DropDown: React.FC = () => {
-  const getChild = async (): Promise<IChild[]> => {
-    const result = await userController.getUser();
+interface IDropDown {
+  value: string;
+  handleChange: (e: { target: { value: string } }) => void;
+  data: IChild[];
+}
+// const getChild = async (): Promise<IChild[]> => {
+//   const result = await userController.getUser();
 
-    return result.user.childs;
-  };
+//   return result.user.childs;
+// };
 
-  const [value, setValue] = useState<string>();
+// const [value, setValue] = useState<string>();
 
-  const [data, dataSet] = useState<IChild[]>([]);
+// const [data, dataSet] = useState<IChild[]>([]);
 
-  useEffect(() => {
-    const setData = async () => {
-      const childs = await getChild();
+// useEffect(() => {
+//   const setData = async () => {
+//     const childs = await getChild();
 
-      dataSet(childs);
-    };
-    setData();
-  }, []);
+//     dataSet(childs);
+//   };
+//   setData();
+// }, []);
 
-  useEffect(() => {
-    data.forEach((child) => {
-      if (value === undefined) {
-        const val = (data as IChild[])[0].name;
-        setValue(val);
-      }
-      if (child.name === value) {
-        localStorage.setItem("currentChild", JSON.stringify(child.id));
-      }
-    });
-  });
-  console.log("1 DropDown = ", value);
+// useEffect(() => {
+//   data.forEach((child) => {
+//     if (value === undefined) {
+//       const val = (data as IChild[])[0].name;
+//       setValue(val);
+//     }
+//     if (child.name === value) {
+//       localStorage.setItem("currentChild", JSON.stringify(child.id));
+//     }
+//   });
+// });
+// console.log("1 DropDown = ", value);
 
-  const handleChange = (e: {
-    target: { value: React.SetStateAction<string> };
-  }) => {
-    setValue(e.target.value as string);
-    data.forEach((child) => {
-      if (child.name === e.target.value) {
-        localStorage.setItem("currentChild", JSON.stringify(child.id));
-      }
-    });
-  };
+// const handleChange = (e: {
+//   target: { value: React.SetStateAction<string> };
+// }) => {
+//   setValue(e.target.value as string);
+//   data.forEach((child) => {
+//     if (child.name === e.target.value) {
+//       localStorage.setItem("currentChild", JSON.stringify(child.id));
+//     }
+//   });
+// };
 
-  return (
-    <div className="dropDown">
-      <select value={value} onChange={handleChange}>
-        {(data as IChild[]).map((item) => (
-          <option key={item.id}>{item.name}</option>
-        ))}
-      </select>
-    </div>
-  );
-};
+const DropDown: React.FC<IDropDown> = ({ value, handleChange, data }) => (
+  <div className="dropDown">
+    <select value={value} onChange={handleChange}>
+      {(data as IChild[]).map((item) => (
+        <option key={item.id}>{item.name}</option>
+      ))}
+    </select>
+  </div>
+);
 
 export default DropDown;
