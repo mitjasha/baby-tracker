d7a14f42cafa5ecb635862cc21111bff40951713
Dzmitry Shakhrai <romantus_@mail.ru>
2022-02-21 19:08:01 +0300

    feat: add modal fast menu button styles

--- src/components/common/ALLModalEdit/ModalEventMenu/BaseModalEventWraper.tsx
+++ src/components/common/ALLModalEdit/ModalEventMenu/BaseModalEventWraper.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import moment from "moment";
+import getEventsChild from "../../../helpers/getEvemtsChild";
 import ModalMenuButton from "../../Buttons/ModalMenuButton/ModalMenuButton";
 import ModalEventMenu from "./ModalEventMenu";
 
@@ -14,22 +16,106 @@ const BaseModalEventWrapper: React.FC<BaseModalWrapperProps> = ({
   if (!isModalVisible) {
     return null;
   }
+  const eventsEat = getEventsChild("Кормление");
+  const eventsActivity = getEventsChild("Активность");
+  const eventsSleep = getEventsChild("Сон");
+  const eventsWalk = getEventsChild("Прогулка");
 
   return (
     <ModalEventMenu onBackdropClick={onBackdropClick}>
       <div className="desktop-modal-container modal-container">
-        <ModalMenuButton eventName="Кормление">
-          <div className="modal-content">Приём пищи</div>
-        </ModalMenuButton>
-        <ModalMenuButton eventName="Активность">
-          <div className="modal-content">Активность</div>
-        </ModalMenuButton>
-        <ModalMenuButton eventName="Сон">
-          <div className="modal-content">Сон</div>
-        </ModalMenuButton>
-        <ModalMenuButton eventName="Прогулка">
-          <div className="modal-content">Прогулка</div>
-        </ModalMenuButton>
+        <div className="buttons-container">
+          <ModalMenuButton eventName="Кормление">
+            <div className="modal-content">
+              <div className="button-icon"></div>
+              <div className="button-text-container">
+                <div style={{ fontWeight: "bold" }}>Приём пищи</div>
+                <div>
+                  {moment(
+                    eventsEat?.sort(
+                      (a, b) =>
+                        Date.parse(b.startTime) - Date.parse(a.startTime),
+                    )[0].endTime,
+                  ).fromNow()}
+                </div>
+              </div>
+              <div className="button-line"></div>
+              <div className="button-plus-container">+</div>
+            </div>
+          </ModalMenuButton>
+          <ModalMenuButton eventName="Активность">
+            <div className="modal-content">
+              <div
+                style={{ background: "#4AA7F1" }}
+                className="button-icon"
+              ></div>
+              <div className="button-text-container">
+                <div style={{ fontWeight: "bold" }}>Активность</div>
+                {eventsActivity?.length && eventsActivity?.length > 0
+                  ? moment(
+                      eventsActivity?.sort(
+                        (a, b) =>
+                          Date.parse(b.startTime) - Date.parse(a.startTime),
+                      )[0].endTime,
+                    ).fromNow()
+                  : ""}
+              </div>
+              <div className="button-line"></div>
+              <div
+                style={{ border: "4px solid #4AA7F1", color: "#4AA7F1" }}
+                className="button-plus-container"
+              >
+                +
+              </div>
+            </div>
+          </ModalMenuButton>
+          <ModalMenuButton eventName="Сон">
+            <div className="modal-content">
+              <div
+                style={{ background: "#C476BC" }}
+                className="button-icon"
+              ></div>
+              <div className="button-text-container">
+                <div style={{ fontWeight: "bold" }}>Сон</div>
+                {moment(
+                  eventsSleep?.sort(
+                    (a, b) => Date.parse(b.startTime) - Date.parse(a.startTime),
+                  )[0].endTime,
+                ).fromNow()}
+              </div>
+              <div className="button-line"></div>
+              <div
+                style={{ border: "4px solid #C476BC", color: "#C476BC" }}
+                className="button-plus-container"
+              >
+                +
+              </div>
+            </div>
+          </ModalMenuButton>
+          <ModalMenuButton eventName="Прогулка">
+            <div className="modal-content">
+              <div
+                style={{ background: "#BEDC0E" }}
+                className="button-icon"
+              ></div>
+              <div className="button-text-container">
+                <div style={{ fontWeight: "bold" }}>Прогулка</div>
+                {moment(
+                  eventsWalk?.sort(
+                    (a, b) => Date.parse(b.startTime) - Date.parse(a.startTime),
+                  )[0].endTime,
+                ).fromNow()}
+              </div>
+              <div className="button-line"></div>
+              <div
+                style={{ border: "4px solid #BEDC0E", color: "#BEDC0E" }}
+                className="button-plus-container"
+              >
+                +
+              </div>
+            </div>
+          </ModalMenuButton>
+        </div>
       </div>
     </ModalEventMenu>
   );
